80be276545dc7b39ff9ec55790e978b5b8e0b8c6
Pedro Magalhaes <pmagalhaes@dminc.com>
2016-10-12 15:28:32 +0700

    Initial commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
   constructor() { }
