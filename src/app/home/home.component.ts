6ed57ac9fbb908131029c3521cceeb64fb4e9edd
Pedro Magalhaes <pmagalhaes@dminc.com>
2016-10-12 17:35:23 +0700

    Configure Sass as global

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styles: [require('./home.component.scss')]
 })
 export class HomeComponent implements OnInit {
 
@@ -13,3 +13,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+ 
